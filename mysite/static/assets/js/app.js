03bcbd1ffd361e7d95fd8544ca8f1285f035fe64
Dullah2k <dullahmwarlabu@gmail.com>
2024-11-19 10:15:41 +0300

    added static assets

---
+++ mysite/static/assets/js/app.js
@@ -0,0 +1,28 @@
+const navToggler = document.getElementById("nav-toggler")
+const nav = document.getElementById("nav")
+const closeNav = document.querySelector(".close-nav")
+
+navToggler.addEventListener("click", () => {
+  nav.classList.add("show-nav")
+})
+
+closeNav.addEventListener("click", ()=>{
+  nav.classList.remove("show-nav")
+})
+
+const observer  = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      entry.target.classList.add('show')
+    } else {
+      entry.target.classList.remove('show')
+    }
+  })
+})
+
+const hiddenElements = document.querySelectorAll('.section')
+hiddenElements.forEach((el) => observer.observe(el))
+
+const options = {
+  theshold: 1.5,
+}
