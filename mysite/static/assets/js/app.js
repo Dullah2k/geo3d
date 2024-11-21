e04cffb2974cdad62a15cd502efa03ed55762b16
Dullah2k <dullahmwarlabu@gmail.com>
2024-11-21 08:11:25 +0300

    setting up static files

--- mysite/static/assets/js/app.js
+++ mysite/static/assets/js/app.js
@@ -10,19 +10,19 @@ closeNav.addEventListener("click", ()=>{
   nav.classList.remove("show-nav")
 })
 
-const observer  = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if(entry.isIntersecting) {
-      entry.target.classList.add('show')
-    } else {
-      entry.target.classList.remove('show')
-    }
-  })
-})
+// const observer  = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     if(entry.isIntersecting) {
+//       entry.target.classList.add('show')
+//     } else {
+//       entry.target.classList.remove('show')
+//     }
+//   })
+// })
 
-const hiddenElements = document.querySelectorAll('.section')
-hiddenElements.forEach((el) => observer.observe(el))
+// const hiddenElements = document.querySelectorAll('.section')
+// hiddenElements.forEach((el) => observer.observe(el))
 
-const options = {
-  theshold: 1.5,
-}
+// const options = {
+//   theshold: 1.5,
+// }
